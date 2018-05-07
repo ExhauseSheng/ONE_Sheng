950856bf375a40859d162b7e2bafb20d83f7183d
Sheng <Sheng@gmail.com>
2018-05-07 22:08:55 +0800

    添加了开始动画和最初测试版的插播图

---
+++ app/src/main/java/com/sheng/one_sheng/activity/StartActivity.java
@@ -0,0 +1,64 @@
+package com.sheng.one_sheng.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+
+import com.sheng.one_sheng.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    private ImageView iv_start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //隐藏标题栏
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        //隐藏状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_start);
+        initImage();
+    }
+
+    private void initImage(){
+        ImageView iv_start = (ImageView) findViewById(R.id.iv_start);
+        iv_start.setImageResource(R.drawable.start);
+        //进行缩放动画
+        ScaleAnimation scaleAnimation = new ScaleAnimation(1.4f, 1f, 1.4f, 1f,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+
+        scaleAnimation.setDuration(4000);       //总共缩放时间为4秒钟
+        //动画播放完成后保持形状
+        scaleAnimation.setFillAfter(true);
+        //监听事件
+        scaleAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(intent);
+                //用于呈现淡入淡出效果
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        //开始动画
+        iv_start.startAnimation(scaleAnimation);
+    }
+}
