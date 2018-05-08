709e9529176b411dee3c0aa31d81579255dd4463
Sheng <Sheng@gmail.com>
2018-05-08 17:12:52 +0800

    新建各子项布局

---
+++ app/src/main/java/com/sheng/one_sheng/activity/MusicActivity.java
@@ -0,0 +1,15 @@
+package com.sheng.one_sheng.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sheng.one_sheng.R;
+
+public class MusicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music);
+    }
+}
