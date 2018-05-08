b9f01724e91d637d9265bc00a1090ee6ae5778e4
Sheng <Sheng@gmail.com>
2018-05-08 20:50:43 +0800

    添加了影视音乐阅读插画的详细内容布局

---
+++ app/src/main/java/com/sheng/one_sheng/adapter/MovieListAdapter.java
@@ -0,0 +1,12 @@
+package com.sheng.one_sheng.adapter;
+
+import android.widget.ArrayAdapter;
+
+import com.sheng.one_sheng.bean.Movie;
+
+/**
+ * Created by 一个傻傻的小男孩 on 2018/5/8.
+ */
+
+public class MovieListAdapter extends ArrayAdapter<Movie> {
+}
---
+++ app/src/main/java/com/sheng/one_sheng/bean/Music.java
@@ -0,0 +1,8 @@
+package com.sheng.one_sheng.bean;
+
+/**
+ * Created by 一个傻傻的小男孩 on 2018/5/8.
+ */
+
+public class Music {
+}
